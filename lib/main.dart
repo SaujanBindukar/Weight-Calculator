756ad8aa7871c631cdd704579103e3010607d0f5
Saujan Bindukar <sauzanbindukar@gmail.com>
2019-07-01 22:42:12 +0545

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:weightcalculator/ui/home.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "Weight on Planet X",
+    home: Home(),
+
+  ));
+}
